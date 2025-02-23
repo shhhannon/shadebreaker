385e42670eb02970918fbec437bd020e6341bc69
shhhannon <shannonbenedicto@yahoo.co.uk>
2025-02-23 21:34:34 +0000

    changed used assets - should work on laptop

---
+++ data/tilesets/items.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="items" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../graphics/objects/items/diamond.png" width="64" height="64"/>
+ </tile>
+ <tile id="1">
+  <image source="../../graphics/objects/items/gold.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="../../graphics/objects/items/potion.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="../../graphics/objects/items/silver.png" width="64" height="64"/>
+ </tile>
+</tileset>
---
+++ data/tilesets/objects.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="115" tileheight="139" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../graphics/objects/crate.png" width="78" height="54"/>
+ </tile>
+ <tile id="1">
+  <image source="../../graphics/objects/door.png" width="115" height="139"/>
+ </tile>
+ <tile id="2">
+  <image source="../../graphics/objects/goblin.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="../../graphics/objects/gunner.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="../../graphics/objects/player.png" width="64" height="64"/>
+ </tile>
+</tileset>
